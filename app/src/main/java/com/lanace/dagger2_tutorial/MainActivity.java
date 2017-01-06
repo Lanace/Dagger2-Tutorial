4d88980b94c58c8719073521b95018e8ef76d6ad
Lanace <lanace93@gmail.com>
2017-01-06 16:19:29 +0900

    use Dependency Injection in Activity

--- app/src/main/java/com/lanace/dagger2_tutorial/MainActivity.java
+++ app/src/main/java/com/lanace/dagger2_tutorial/MainActivity.java
@@ -2,12 +2,28 @@ package com.lanace.dagger2_tutorial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.lanace.dagger2_tutorial.Component.DaggerVehicleComponent;
+import com.lanace.dagger2_tutorial.Component.VehicleComponent;
+import com.lanace.dagger2_tutorial.Module.VehicleModule;
+import com.lanace.dagger2_tutorial.model.Vehicle;
 
 public class MainActivity extends AppCompatActivity {
+
+  Vehicle vehicle;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    VehicleComponent component = DaggerVehicleComponent
+        .builder()
+        .vehicleModule(new VehicleModule())
+        .build();
+
+    vehicle = component.provideVehicle();
+    Toast.makeText(this, "Speed: " + vehicle.getSpeed(), Toast.LENGTH_SHORT).show();
   }
 }
